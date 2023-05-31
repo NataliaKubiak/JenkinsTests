deeef2eeb20717df53be9f4fe7da96f8f9c8254c
Natalia Kubiak <nataliia.kubiak@gmail.com>
2023-05-31 14:00:34 +0200

    Errors correction + add comments in MainPage

--- src/test/java/org/portfolio/models/MainPage.java
+++ src/test/java/org/portfolio/models/MainPage.java
@@ -1,16 +1,18 @@
 package org.portfolio.models;
 
 import org.openqa.selenium.WebDriver;
-import org.portfolio.models.base.BasePage;
-import org.portfolio.models.component.MainHeaderComponent;
+import org.portfolio.models.base.BaseMainHeaderPage;
 
-public class MainPage extends BasePage<MainHeaderComponent> {
+//extends BasePage - I have there <Header> that's why I need to mention Header here too
+//set a <MainHeaderComponent> to get this particular header through getHeader()
+
+//after creating BaseMainHeaderPage inherit from it and remove overridden getHeader()
+
+public class MainPage extends BaseMainHeaderPage {
+
     public MainPage(WebDriver driver) {
         super(driver);
     }
 
-    @Override
-    public MainHeaderComponent getHeader() {
-        return new MainHeaderComponent(getDriver()); //don't see why it doesn't work without casting
-    }
+    //action methods
 }
--- src/test/java/org/portfolio/models/base/BaseHeaderComponent.java
+++ src/test/java/org/portfolio/models/base/BaseHeaderComponent.java
@@ -3,7 +3,7 @@ package org.portfolio.models.base;
 import org.openqa.selenium.WebDriver;
 
 //заготовка for actual Header
-public class BaseHeaderComponent extends BaseModel {
+public abstract class BaseHeaderComponent extends BaseModel {
 
     public BaseHeaderComponent(WebDriver driver) {
         super(driver);
